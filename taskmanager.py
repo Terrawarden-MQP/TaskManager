7f914663d2d54bf8b51b35a85c7ef7d50d2e89fe
S Taylor <sataylor@wpi.edu>
2025-02-05 10:04:39 -0500

    prepped for basic integration
    Co-authored-by: Kevin Siegall <ksiegall@users.noreply.github.com>

--- taskmanager.py
+++ taskmanager.py
@@ -4,9 +4,9 @@ from rclpy.node import Node
 
 # CV Bridge and message imports
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
-from vision_msgs.msg import ObjectHypothesisWithPose, BoundingBox2D, Detection2D, Detection2DArray
-from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import String, Boolean
+from vision_msgs.msg import Detection2D
+from geometry_msgs.msg import Point2D, PoseWithCovariance, PoseStamped
 
 import cv2
 import numpy as np
@@ -26,36 +26,124 @@ class TaskManagerNode(Node):
     def __init__(self):
         super().__init__('trt_detection_node')
 
-        # Create a subscriber to the Image topic
-        # topic can be changed through parameters in ros2 launch command
-        self.declare_parameter('topic', 'image')
-        topic_name = self.get_parameter('topic')
-        self.subscription = self.create_subscription(Image, topic_name.value, self.listener_callback, 10)
-        self.detection_publisher = self.create_publisher(Detection2DArray, 'trt_detection', 10)
+        ### TOPIC DECLARATION - ALL PARAMETERIZED THROUGH ROS2 LAUNCH
+
+        # Topic for receiving raw image from camera
+        self.declare_parameter('image_topic', 'image')
+        # Topic for receiving Detection from LiveDetect Node
+        self.declare_parameter('detection_topic', 'joisie_detection')
+        # Topic for sending detected object centroid to VBM
+        self.declare_parameter('centroid_topic', 'joisie_detected_centroid')
+        # Topic for receiving Telemetry from Drone Node
+        # self.declare_parameter('telemetry', 'joisie_telemetry')
+        # Topic for sending target pose to drone
+        self.declare_parameter('drone_pose_topic', 'joisie_target_pose')
+        # Topic for receiving grasp from VBM
+        self.declare_parameter('vbm_grasp_topic', 'joisie_grasp_read')
+        # Topic for sending grasp to arm
+        self.declare_parameter('arm_grasp_topic', 'joisie_grasp_send')
+        # CustomArmMsg from Arm Node
+        self.declare_parameter('arm_status_topic', 'joisie_arm_status')
+        # Topic for InRangeOfObj Service Call to Arm Node
+        self.declare_parameter('arm_service_topic', 'joisie_arm_inrange_service')
+
+        self.declare_parameter('state_setter_topic', 'joisie_set_state')
+
+        self.state_setter_subscriber = self.create_subscription(String, 
+                                            self.get_parameter('state_setter_topic').value, 
+                                            self.receive_desired_state, 10)
+        self.image_subscriber = self.create_subscription(Image, 
+                                                    self.get_parameter('image_topic').value, 
+                                                    self.receive_raw_image, 10)
+        self.detection_subscriber = self.create_subscription(Detection2D, 
+                                                    self.get_parameter('detection_topic').value, 
+                                                    self.receive_detection, 10)
+        # self.telemetry_subscriber = self.create_subscription(Telemetry, 
+        #                                             self.get_parameter('telemetry_topic').value, 
+        #                                             self.receive_detection, 10)
+        self.grasp_subscriber = self.create_subscription(PoseStamped, 
+                                                    self.get_parameter('vbm_grasp_topic').value, 
+                                                    self.receive_grasp, 10)
+        # self.arm_status_subscriber = self.create_subscription(DynaArmStatus, 
+        #                                             self.get_parameter('arm_status_topic').value, 
+        #                                             self.receive_grasp, 10)
+
+        self.drone_publisher = self.create_publisher(PoseStamped, 
+                                                    self.get_parameter('drone_pose_topic').value, 10)
+        self.centroid_publisher = self.create_publisher(Point2D, 
+                                                    self.get_parameter('centroid_topic').value, 10)
+        self.grasp_publisher = self.create_publisher(PoseStamped, 
+                                                    self.get_parameter('arm_grasp_topic').value, 10)
+
+        self.in_range_service = self.create_client(Boolean, self.get_parameter('arm_grasp_topic').value)
 
         self.state = State.HOLD
-        # C
 
-    def onReceive(self, ros_msg):
-        #
-        pass
+    def receive_desired_state(self, ros_msg: String):
+        bindings = {
+            "HOLD": State.HOLD,
+            "SEARCH": State.SEARCHING,
+            "NAV": State.NAVIGATING,
+            "GRASP": State.GRASPING,
+            "DEPOSIT": State.DEPOSITING
+        }
+        try:
+            string = ros_msg.data
+            self.state = bindings[string.upper()]
+        except:
+            print("No matching state for string:", string)
 
-    def hold(self):
-        pass
-    
-    def search(self):
-        # flies in whatever search pattern we decide upon
-        # when an object is detected, move to navigate state
-        pass
+    def receive_detection(self, ros_msg: Detection2D):
+        self.detection = ros_msg
 
-    def navigate(self):
-        # navigates drone to be within range of object
-        # if object is within range, move to grasp state
-        # if object is lost, return to search state
+    def receive_raw_image(self, ros_msg: Image):
+        self.raw_image = ros_msg
+
+    # def receive_telemetry(self, ros_msg: Telemetry):
+    #     self.telemetry = ros_msg
+
+    def receive_grasp(self, ros_msg: PoseStamped):
+        self.raw_grasp = ros_msg
+
+    # def receive_arm_status(self, ros_msg: DynaArmStatus):
+    #     self.arm_status = ros_msg
+
+
+
+    # NONBLOCKING CODE YOU FOOOOOOOOOL
+
+    def hold(self):  
+        # tell the drone to hold position
+        
+        # listen for desiredState
+        # if there is desiredState, state = desiredState
+        if self.desired_state:
+            self.State = desired_state
+            self.desired_state = None
         pass
 
     def grasp(self):
-        # generates and exectues object grasp
+        # hold drone current position
+        
+        # calculate grasp
+        # check livedetect information
+        # if bounding box size is within 'pickup' range AND bounding box centroid is within 'pickup' range
+            # execute grasp
+        pass
+    
+    def search(self):
+        # move drone to next position in search pattern
+        # if new/different data from LiveDetect
+            # state = navigate
+        pass
+
+    def navigate(self):
+        # move drone to next position in navigation sequence
+        # check livedetect information
+        # if bounding box size is within 'pickup' range AND bounding box centroid is within 'pickup' range
+            # state = grasp
+        # if no object is detected for X out of Y LiveDetect messages
+            # state = search
         pass
     
     def deposit(self):
@@ -64,6 +152,8 @@ class TaskManagerNode(Node):
         pass
 
     def checkForErrors(self):
+        # Read most recent Telemetry and ArmStatus data
+        # Set mode to State.HOLD if any errors
         pass
 
     def main(self):
@@ -74,12 +164,20 @@ class TaskManagerNode(Node):
             if self.state == State.HOLD:
                 new_state = self.hold()
             elif self.state == State.SEARCHING:
+                # self.process_detection()
                 new_state = self.search()
             elif self.state == State.NAVIGATING:
+                # self.process_detection()
+                # self.process_grasp()
                 new_state = self.navigate()
             elif self.state == State.GRASPING:
+                # self.process_detection()
+                # self.process_grasp()
                 new_state = self.grasp()
             elif self.state == State.DEPOSITING:
                 new_state = self.deposit()
             
+            if self.checkForErrors():
+                new_state = State.HOLD
+
             self.state = new_state
