91d21a9015e62870dc906c2abd29e1eab3d17732
S Taylor <sataylor@wpi.edu>
2025-04-13 12:51:42 -0400

    test_is_in_range_NED done

--- joisie_manager/test/test_method.py
+++ joisie_manager/test/test_method.py
@@ -23,10 +23,11 @@ from ..joisie_manager.taskmanager import *
 from .test_helpers import *
 
 @pytest.fixture
-def rclpy_sucks():
+def manager():
     # Runs before tests
     rclpy.init()
-    yield
+    manager = TaskManagerNode()
+    yield manager
     # Runs after tests
     rclpy.shutdown()
 
@@ -38,53 +39,80 @@ def get_state_setter(task_manager):
 
 # Function name starts with "test_"
 @pytest.mark.skip(reason="not implemented")
-def test_math(rclpy_sucks):
+def test_math(manager):
     assert 2 + 2 == 5   # This should fail for most mathematical systems\
 
 @pytest.mark.skip(reason="not implemented")
-def test_send_waypoint_NED(rclpy_sucks):
+def test_send_waypoint_NED(manager):
+    assert 2 + 2 == 5  
+
+# @pytest.mark.skip(reason="not implemented")
+def test_offset_point_FLU(manager):
+
+    FLUpoint = create_point(1,1,1)
+
+    FLUoffset = create_point(0, 0, 0)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1, 1, 1) # + none
+
+    FLUoffset = create_point(0.5, 0, 0)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1.5, 1, 1) # + X
+
+    FLUoffset = create_point(0, 0.5, 0)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1, 1.5, 1) # + Y
+
+    FLUoffset = create_point(0, 0, 0.5)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1, 1, 1.5) # + Z
+
+    FLUoffset = create_point(0.5, 0.5, 0.5)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1.5, 1.5, 1.5) # + all
+
+    FLUoffset = create_point(-0.5, 0, 0)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(.5, 1, 1) # - X
+
+    FLUoffset = create_point(0, -0.5, 0)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1, .5, 1) # - Y
+
+    FLUoffset = create_point(0, 0, -0.5)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(1, 1, .5) # - Z
+
+    FLUoffset = create_point(-0.5, -0.5, -0.5)
+    assert manager.offsetPointFLU(FLUpoint, FLUoffset) == create_point(0.5, 0.5, 0.5) # - all
+
+@pytest.mark.skip(reason="not implemented")
+def test_FLU_to_NED(manager):
     assert 2 + 2 == 5  
 
 @pytest.mark.skip(reason="not implemented")
-def test_offset_point_FLU(rclpy_sucks):
+def test_FLU_to_NED_quaternion(manager):
     assert 2 + 2 == 5  
 
-@pytest.mark.skip(reason="not implemented")
-def test_FLU_to_NED(rclpy_sucks):
-    assert 2 + 2 == 5  
-
-@pytest.mark.skip(reason="not implemented")
-def test_FLU_to_NED_quaternion(rclpy_sucks):
-    assert 2 + 2 == 5  
-
-def test_is_in_range_NED(rclpy_sucks):
-    manager = TaskManagerNode()
+def test_is_in_range_NED(manager):
+    # make publisher
+    pub = get_publisher(manager, DroneTelemetry, "drone_telemetry_topic")
 
     dronePoint = [0,0,0]
-    mock_telemetry(dronePoint) #only care about position here
-    # make publisher
-    # publish mock_telemetry
+    msg = mock_telemetry(dronePoint) #only care about position here
+    
+    pub. publish(msg)
 
     refRange = create_point(1.2, 0, 1.2)
-    assert manager.isInRangeNED(refRange, 1.1, 1.5) == False #out of range X
-    assert manager.isInRangeNED(refRange, 1.5, 1.1) == False #out of range Z
+    assert manager.isInRangeNED(refRange, 1.1, 1.5) == False #out of range +X
+    assert manager.isInRangeNED(refRange, 1.5, 1.1) == False #out of range +Z
     assert manager.isInRangeNED(refRange, 1.5, 1.5) == True #in range X, Y, Z
 
     refRange = create_point(0, 1.2, 0)
-    assert manager.isInRangeNED(refRange, 1.1, 1) == False #out of range Y
+    assert manager.isInRangeNED(refRange, 1.1, 1) == False #out of range +Y
     assert manager.isInRangeNED(refRange, 1.5, 1.5) == True #in range X, Y, Z
 
-
-
 @pytest.mark.skip(reason="not implemented")
-def test_quaternion_to_heading():
+def test_check_for_errors(manager):
     assert 2 + 2 == 5  
 
 @pytest.mark.skip(reason="not implemented")
-def test_check_for_errors():
+def test_check_failsafe(manager):
     assert 2 + 2 == 5  
 
-@pytest.mark.skip(reason="not implemented")
-def test_check_failsafe():
+@pytest.mark.skip(reason="not used - not priority")
+def test_quaternion_to_heading(manager):
     assert 2 + 2 == 5  
 
